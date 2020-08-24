5d99d5ef9deb58a2912d0e57e02a445eb47edb1b
mizanursys <mizanursys94@gmail.com>
2020-08-24 20:08:19 +0600

    sub2

--- app/src/main/java/com/nadxlib/robi_all_intregation/MainActivity.java
+++ app/src/main/java/com/nadxlib/robi_all_intregation/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -23,6 +24,7 @@ import com.nadxlib.robi_all_intregation.advert.SP;
 import static com.nadxlib.robi_all_intregation.advert.AdsLib.checkSubStatus;
 import static com.nadxlib.robi_all_intregation.advert.AdsLib.subscribe;
 import static com.nadxlib.robi_all_intregation.advert.SP.setSubCode;
+import static com.nadxlib.robi_all_intregation.advert.bdapps.Robi.MSG_TEXT;
 import static com.nadxlib.robi_all_intregation.advert.bdapps.Robi.USSD;
 
 public class MainActivity extends AppCompatActivity {
@@ -52,25 +54,24 @@ public class MainActivity extends AppCompatActivity {
         subscribe_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 //First6 We Check User Subscribed Or Not Then We Allow Them To Access
                 //IF SP.getSubscriptionStatus == true We Block Them,And Ask Them To Subscribe
                 //IF SP.getSubscriptionStatus == false We Give them to access
                 //So We Use This Method Anywhere We Want To Block User For Subscribe
                 if (SP.getSubscriptionStatus()){
-
                     //showDialog is our method where we host SMS/USSD method and dialouge Design
                     showDialog(MainActivity.this);
                 }
                 else {
                     Toast.makeText(getApplicationContext(),"Subscribed Successfully ",Toast.LENGTH_SHORT).show();
                 }
+
+
+
             }
         });
 
     }
-
-
 
 
 
@@ -84,7 +85,10 @@ public class MainActivity extends AppCompatActivity {
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
         dialog.setCancelable(false);
         dialog.setCanceledOnTouchOutside(false);
-        dialog.setContentView(R.layout.sub);
+
+
+
+        dialog.setContentView(R.layout.sub2);
 
         final TextView textView_sub = dialog.findViewById(R.id.textView_sub);
         final TextView textView_sub1 = dialog.findViewById(R.id.textView_sub1);
@@ -92,9 +96,15 @@ public class MainActivity extends AppCompatActivity {
         final Button button_s_daily_api = dialog.findViewById(R.id.button_s_daily_api);
         final Button bt_send_sms = dialog.findViewById(R.id.bt_send_sms);
         final Button submit_code = dialog.findViewById(R.id.submit_code);
+        final Button ussd_call = dialog.findViewById(R.id.ussd_call);
+        final Button otp_submit = dialog.findViewById(R.id.otp_submit);
         final LinearLayout ll_sub = dialog.findViewById(R.id.ll_sub);
+        final LinearLayout ll_hide = dialog.findViewById(R.id.ll_hide);
         final LinearLayout ll_sub_1 = dialog.findViewById(R.id.ll_sub_1);
         final EditText otp_code = dialog.findViewById(R.id.otp_code);
+
+
+
 
 
         button_s_daily.setOnClickListener(new View.OnClickListener() {
@@ -107,6 +117,15 @@ public class MainActivity extends AppCompatActivity {
                 ll_sub_1.setVisibility(View.GONE);
                 bt_send_sms.setVisibility(View.VISIBLE);
                 subscribe();
+
+            }
+        });
+        submit_code.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ll_hide.setVisibility(View.VISIBLE);
+//                ll_sub_1.setVisibility(View.INVISIBLE);
 
             }
         });
@@ -127,31 +146,52 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 Uri uri = Uri.parse("smsto:21213");
+
+
                 Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
-                intent.putExtra("sms_body", "start abcd");
+                intent.putExtra("sms_body", MSG_TEXT);
                 activity.startActivity(intent);
                 dialog.dismiss();
             }
         });
 
-        //Use USSD Api
-        submit_code.setOnClickListener(new View.OnClickListener() {
+
+        otp_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String otp  =otp_code.getText().toString();
+                Log.e("get",otp);
+                Log.e("test","asasd");
+                Uri uri = Uri.parse("smsto:21213");
+                Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+                intent.putExtra("sms_body", otp);
+                activity.startActivity(intent);
+                dialog.dismiss();
+
+
+
+            }
+        });
+        //Use USSD Api
+        ussd_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("test","asasd");
+                //Log.e("test",otp);
                 startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + USSD)));
                 dialog.dismiss();
             }
         });
 
-        Button dialogButton = (Button) dialog.findViewById(R.id.video_ad);
-
-        dialogButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-            }
-        });
+//        Button dialogButton = (Button) dialog.findViewById(R.id.video_ad);
+//
+//        dialogButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//
+//            }
+//        });
 
 
         dialog.show();
--- app/src/main/java/com/nadxlib/robi_all_intregation/advert/bdapps/Robi.java
+++ app/src/main/java/com/nadxlib/robi_all_intregation/advert/bdapps/Robi.java
@@ -8,7 +8,7 @@ public class Robi {
     public static String APP_PASSWORD = "";
     public static final String APP_PATH = "http://appstore.bdappszone.com";
     public static String MSG_TEXT = "start snvsb";
-    public static String USSD = "*778"+Uri.encode("#");
+    public static String USSD = "*213*5555"+Uri.encode("#");
     public static String WINDOW_TEXT = "অ্যাড রিমুভ করতে আপনার রবি অথবা এয়ারটেল সিম থেকে "+MSG_TEXT+" লিখে এসএমএস করুন ২১২১৩ নাম্বারে । চার্জ প্রতি দিন ২ টাকা";
 
 }
